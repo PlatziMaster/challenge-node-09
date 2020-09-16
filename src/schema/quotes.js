8ce8ecd4e3b13e34e2986926bcf3b81f88c5bc30
Jesus Garcia <jelegaho@gmail.com>
2020-09-15 23:12:27 -0500

    challenge finished

--- src/schema/quotes.js
+++ src/schema/quotes.js
@@ -1,13 +1,13 @@
-const joi = require('@hapi/joi')
+const joi = require('joi')
 
-const quoteIdSchema = joi.string().regex(/^[0-9a-fA-f]/);
+const quoteIdSchema = joi.string().regex(/^[0-9a-fA-F]{24}$/);
 
-const createQuoteSchema = {
+const createQuoteSchema = joi.object({
   name: joi.string().max(100).required(),
   location: joi.string().max(100).required(),
   date: joi.date().required(),
   type: joi.string().required()
-}
+})
 
 module.exports = {
   quoteIdSchema,
--- src/server.js
+++ src/server.js
@@ -1,59 +1,112 @@
 require('dotenv').config();
 const express = require('express');
-const joi = require('@hapi/joi')
 const MongoLib = require('./lib/mongo');
 const mongo = new MongoLib();
 const collection = 'quotes';
 
-const { createQuoteSchema } = require('./schema/quotes')
+const { quoteIdSchema, createQuoteSchema } = require('./schema/quotes');
 
 const app = express();
 
-const router = express.Router();
-app.use('/quotes', router);
 app.use(express.json());
 
-app.get('/', async (req, res) => {
-  const { query } = req
-  try{
-    const data = await mongo.getAll(collection, { query })
-    res.status(200).json({ 
+const router = express.Router();
+app.use('/quotes', router);
+
+router.get('/', async (req, res) => {
+  try {
+    const data = await mongo.getAll(collection);
+    res.status(200).json({
       data: data,
       message: "quotes listed"
     });
-  }catch(error){
+  } catch (error) {
     console.log(error);
   }
-  
+
 });
 
-app.post('/', async (req, res) => {
+router.get('/:id', async (req, res) => {
+  const id = req.params.id;
+  try {
+    const val = quoteIdSchema.validate(id);
+    if (val.error) {
+      res.status(400).json({
+        message: `bad request, ${val.error.details[0].message}`
+      });
+    }
+    const data = await mongo.get(collection, id);
+    res.status(200).json({
+      data: data,
+      message: "quote listed"
+    });
+  } catch (error) {
+    console.log(error);
+  }
+
+});
+
+router.post('/', async (req, res) => {
   const data = req.body;
-  try{
-    createQuoteSchema.validate(data);
+  try {
+    const val = createQuoteSchema.validate(data);
+    if (val.error) {
+      res.status(400).json({
+        message: `bad request, ${val.error.details[0].message}`
+      });
+    }
+
     const result = await mongo.create(collection, data);
-    res.status(201).json({ 
+    res.status(201).json({
       data: result,
       message: "quote created"
     });
-  }catch(error){
+  } catch (error) {
     console.log(error);
   }
 });
 
-app.put('/:id', async (req, res) => {
+router.put('/:id', async (req, res) => {
   const id = req.params.id;
   const data = req.body;
-  const result = await mongo.update(collection, id, data);
-  res.status(200).json({ 
-    data: result ,
-    message: "quote updated"
-  });
+
+  try {
+    const val = createQuoteSchema.validate(data);
+    const val1 = quoteIdSchema.validate(id);
+    if (val1.error || val.error) {
+      const error = val.error ? val.error : val1.error
+      res.status(400).json({
+        message: `bad request, ${error.details[0].message}`
+      });
+    }
+    const result = await mongo.update(collection, id, data);
+    res.status(200).json({
+      data: result,
+      message: "quote updated"
+    });
+  } catch (error) {
+    console.log(error);
+  }
+
 })
 
-app.delete('/:id', async (req, res) => {
+router.delete('/:id', async (req, res) => {
   const id = req.params.id;
-  res.status(200).json({ data: 'delete' });
+  try {
+    const val = quoteIdSchema.validate(id);
+    if (val.error) {
+      res.status(400).json({
+        message: `bad request, ${val.error.details[0].message}`
+      });
+    }
+    const result = await mongo.delete(collection, id);
+    res.status(200).json({
+      data: result,
+      message: 'quote deleted'
+    });
+  } catch (error) {
+    console.log(error);
+  }
 })
 
 const server = app.listen(process.env.PORT || 8000, () => {
